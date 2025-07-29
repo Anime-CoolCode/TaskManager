0ec6fcade3cc0246741a45dcfdb3095d5b54364a
Animesh Singh <singh.animesh.tanwar@gmail.com>
2025-07-30 00:19:16 +0530

    First Run, learning basics_2

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MyApp',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const MyWidget(),
     );
   }
 }
 
-class MyWidget extends StatelessWidget{
+class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("My app", 
+        title: const Text(
+          "My app",
           style: TextStyle(
             fontWeight: FontWeight.bold,
             color: Colors.blue,
-            fontSize: 25
+            fontSize: 25,
           ),
         ),
         centerTitle: true,
       ),
       body: Padding(
         padding: EdgeInsets.all(20),
-        child : Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.blue.shade200 , Colors.blueAccent.shade100])
+          ),
+          child:
             Card(
               elevation: 10,
               shape: RoundedRectangleBorder(
@@ -49,24 +49,32 @@ class MyWidget extends StatelessWidget{
               ),
               shadowColor: Colors.blueAccent.withOpacity(0.3),
               child: Padding(
-                  padding: const EdgeInsets.all(24),
-                  child: Column(
-                    children: [
-                      Text('Welcome to Anime Studio',
+                padding: EdgeInsets.all(24),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Welcome to Anime Studio',
                       style: TextStyle(
                         fontSize: 25,
                         fontWeight: FontWeight.bold,
-                        color : Colors.black
+                        color: Colors.black,
                       ),
-                        textAlign: TextAlign.center,
-                      )
-                      
-                      //Image(image: Ass)
-                    ],
-                  ),
+                      textAlign: TextAlign.center,
+                    ),//khggh
+                    const SizedBox(height: 20,),
+                    const Image(image: AssetImage('assets/logo.png')),
+                    const SizedBox(height: 20,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(onPressed: (){}, child: Text("Yes")),
+                        ElevatedButton(onPressed: (){}, child: Text("No")),
+                      ],
+                    )
+                  ],
+                ),
               ),
-            )
-          ],
+            ),
         ),
       ),
     );
